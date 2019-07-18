90bcf0c6414a448285fde9bb64ce440342ffc625
zenott <mblevel@gmail.com>
2019-07-18 09:28:43 +0200

    Add analyze

---
+++ array-analysis/analyze.js
@@ -0,0 +1,10 @@
+function analyze(array) {
+  return {
+    average: array.reduce((acc, curr) => acc + curr, 0) / array.length,
+    min: Math.min(...array),
+    max: Math.max(...array),
+    length: array.length
+  };
+}
+
+module.exports = analyze;
